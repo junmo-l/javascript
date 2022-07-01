3c21ea5f00b84fd3bec05f50966073cdc5ec1d9e
junmo-l <wlq1502@gmail.com>
2022-07-01 19:35:37 +0900

    commit

---
+++ baekjoonLevelTest.js
@@ -0,0 +1,62 @@
+// 텍스트불러오기
+// const fs = require('fs');
+// const input = fs.readFileSync("/dev/stdin").toString().trim();
+// let A = input;
+
+// 1단계 1330 조건문 두 수 비교하기
+//파일 불러오기
+// const fs = require('fs');
+// const input = fs.readFileSync(0,'utf8').toString().split(' ');
+
+// //파일의 첫번째 값
+// let A = Number(input[0]);
+// //파일의 두번째 값
+// let B = Number(input[1]);
+
+// if(A>B){
+//     console.log(">");
+// } else if (A<B){
+//     console.log("<")
+// } else {
+//     console.log("==")
+// }
+
+// `${A+B}??!`
+// ""
+
+// // 2단계 9498 시험 성적
+// const fs = require('fs');
+// const input = fs.readFileSync(0,'utf8').toString();
+
+// let score = Number(input[0]);
+
+// if (score >= 90) {
+//     console.log("A")
+// } else if (score >= 80) {
+//     console.log("B")
+// } else if (score >= 70) {
+//     console.log("C")
+// } else if (score >= 60) {
+//     console.log("D")
+// } else  {
+//     console.log("F")
+// }
+
+// 3단계 9498 윤년
+const fs = require('fs');
+const input = fs.readFileSync(0,'utf8').toString();
+
+let year = Number(input);
+
+if ((year%4===0 && year%100!==0) || year%400===0) {
+    console.log("1");
+} else {
+    console.log("0");
+}
+
+
+// let A = 1 <= 4000
+// let year = 2000;
+// !B%100==0
+
+// let year = 2000;
--- ifs.js
+++ ifs.js
@@ -1,25 +1,25 @@
 // ()안에 내용이 맞으면 스코프 실행
 // 상수 보다는 변수를 많이 사용
 
-let condition = false;
-condition = 30 > 6;
+// let condition = false;
+// condition = 30 > 6;
 
-numberFirst = 30;
-numberSecond = 6;
-condition = numberFirst > numberSecond;
+// numberFirst = 30;
+// numberSecond = 6;
+// condition = numberFirst > numberSecond;
 
-let conditionSecond = (numberFirst <= numberSecond);
-condition = condition || conditionSecond;
+// let conditionSecond = (numberFirst <= numberSecond);
+// condition = condition || conditionSecond;
 
-if (condition) {
-    console.log("in if state!")
-}
+// if (condition) {
+//     console.log("in if state!")
+// }
 
-if (!condition) {
-    console.log("not in if state!")
-}
+// if (!condition) {
+//     console.log("not in if state!")
+// }
 
-let score = 50;
+let score = 90;
 if (score >= 90) {
     console.log("well !")
 } else if (score >= 50) {
