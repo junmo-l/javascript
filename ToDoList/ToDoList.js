9cd8dfcebbd1c509512c2786a393e95f2f9f0f85
junmo-l <wlq1502@gmail.com>
2022-07-06 23:01:40 +0900

    commit

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,30 +1,94 @@
-let inputText = document.querySelector("#inputext");
+
+
+// addButton.addEventListener('click', () =>{
+//     let hText = inputText.value;
+
+//     let li = document.createElement('li');
+//     li.setAttribute('class', 'item');
+
+//     let spanTask = document.createElement('span');
+//     spanTask.setAttribute('class', 'task');
+//     spanTask.innerText = hText;
+//     li.appendChild(spanTask);
+
+//     let spanFunction = document.createElement('span');
+//     spanFunction.setAttribute('class', 'span_function');
+//     li.appendChild(spanFunction)
+// });
+
+
+
+
+// let addEvent = document.querySelector("#add_button");
+// addEvent.addEventListener('click', addItem);
+
+
+// function add(first=""){
+//     console.log(`add_button : ${first}`);
+//     let ulTag = document.querySelector('ul');
+//     let liTag = document.createElement('li');
+//     let liText = document.createTextNode('후..');
+//     liTag.appendChild(liText);
+//     ulTag.appendChild(liTag);
+//     return true;
+// }
+
+// function remove(first=""){
+//     console.log(`add_button : ${first}`);
+//     let ultag = document.querySelector('ul');
+//     ultag.lastChild.remove();
+//     return true;
+// }
+
+// let addEvent = document.querySelector("#add_button");
+// addEvent.addEventListener('click', add);
+
+// let removeEvent = document.querySelector("#remove");
+// removeEvent.addEventListener('click', remove);
+
 let addButton = document.querySelector("#add_button");
+let inputText = document.querySelector("#inputext");
 let ulItems = document.querySelector("#ul_items");
 
-function addItem(first) {
-    console.log(`add_button : ${first}`)
-    let 
-    return true;
-}
+addButton.addEventListener('click', ()=>{
+    let todoText = inputText.value;
 
-function printName()  {
-    let name = document.getElementById('inputext').value;
-    document.getElementById("result").innerText = name;
-  }
+    let li = document.createElement('li');
+    li.setAttribute('class', 'li_item');
 
-function add(first=""){
-    console.log(`add_button : ${first}`)
-    let ulTag = document.querySelector('ul');
-    let liTag = document.createElement('li');
-    let liText = document.createTextNode('Sony');
-    liTag.appendChild(liText);
-    ulTag.appendChild(liTag);
-    return true;
-}
+    let spanTask = document.createElement('span');
+    spanTask.setAttribute('class', 'task');
+    spanTask.innerText = todoText;
+    li.appendChild(spanTask);
 
-let addEvent = document.querySelector("#add_button");
-addEvent.addEventListener('click', add);
+    let spanFunction = document.createElement('span');
+    spanFunction.setAttribute('class', 'span_function');
+    li.appendChild(spanFunction);
 
-let removeEvent = document.querySelector("#remove");
-removeEvent.addEventListener('click', remove);
+    let spanLike = document.createElement('span');
+    spanLike.setAttribute('class', 'like');
+    spanFunction.appendChild(spanLike);
+
+    let iFavor = document.createElement('i');
+    iFavor.setAttribute('class', 'i_icons');
+    iFavor.innerText = 'favorite';
+    spanLike.appendChild(iFavor);
+
+    let spanDelete = document.createElement('span');
+    spanDelete.setAttribute('class', 'delete');
+    spanFunction.appendChild(spanDelete);
+
+    let iDelete = document.createElement('i');
+    iDelete.setAttribute('class', 'i_icons');
+    iDelete.innerText = 'delete';
+    spanDelete.appendChild(iDelete);
+
+    ulItems.appendChild(li);
+
+    inputText.value = '';
+
+    spanDelete.addEventListener('click', ()=>{
+        li.remove();
+    });
+
+});
